8b1c674b5833136cd638536c3113ea017b4ec14a
Mica CF <mcohenfuentes@gmail.com>
2025-01-23 17:40:02 -0500

    saving mid exercise 3

--- app.js
+++ app.js
@@ -6,6 +6,50 @@ Exercise 1: Define an empty array
 Exercise 1 has been completed for you:
 */
 
-const foods = [];  
+//const foods = [];  
 
-console.log('Exercise 1 result:', foods);
+//console.log('Exercise 1 result:', foods);
+
+
+
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+const foods = ['pizza', 'cheeseburger'];
+
+console.log(foods);
+
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+console.log(foods.unshift('taco'))
+
+console.log(foods)
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+console.log(foods[1])
+
+
